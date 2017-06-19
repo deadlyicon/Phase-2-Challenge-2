de3ae80e00d5b9e9c8cb2d7cb08ab632b5060307
Doug L <douglubaway@gmail.com>
2017-06-19 10:33:55 -0700

    Established files, began Part 1 of Challenge

---
+++ Part-1/functions.js
@@ -0,0 +1,27 @@
+### Show a weekday
+function weekday(year, month, day) {
+  var number = getDay(year, month, day);
+  if (number = 1) {
+    return "Monday";
+  } else if (number = 2){
+    return "Tuesday";
+  } else if (number = 3){
+    return "Wednesday";
+  } else if (number = 4){
+    return "Thursday";
+  } else if (number = 5){
+    return "Friday";
+  } else if (number = 6){
+    return "Saturday";
+  } else if (number = 7){
+    return "Sunday";
+  }
+}
+
+weekday(2017, 6, 19);
+
+### Get a snippet from text
+
+### Number of properties
+
+### Filter between
